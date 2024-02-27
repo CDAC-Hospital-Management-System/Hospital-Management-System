fe1223cd99fc82a7bed43de113f0e863a4890f97
Satyajeet Mohite <mohitesatyajeet7@gmail.com>
2024-02-27 12:14:57 +0530

    Mail Service BacKEnd Accountant Receptionist Front End Added

---
+++ hospital-management-server/src/main/java/com/sunbeam/MailService.java
@@ -0,0 +1,19 @@
+//package com.sunbeam;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.mail.MailSender;
+//import org.springframework.mail.javamail.JavaMailSenderImpl;
+//import org.springframework.stereotype.Component;
+//
+//import com.sunbeam.services.EmailSenderService;
+
+//@Component
+//public class MailService {
+//	
+//	@Bean
+//	public MailSender getMailSender()
+//	{
+//		return new JavaMailSenderImpl();
+//	}
+//
+//}
